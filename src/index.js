abc862ba7a771c055aa9f165795dfe65216d495e
wolf3d <oldcar@inbox.lv>
2020-09-17 23:38:36 +0300

    set up the initial state for the Game component

--- src/index.js
+++ src/index.js
@@ -71,6 +71,15 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }
+      ],
+    };
+  }
   render() {
     return (
       <div className="game">
