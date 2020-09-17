6aa83a1babfe9c7e1781bae56ab4d75d86f83f3e
wolf3d <oldcar@inbox.lv>
2020-09-17 22:47:09 +0300

    pass down a function from the Board to the Square

--- src/index.js
+++ src/index.js
@@ -29,7 +29,10 @@ class Board extends React.Component {
     };
   }
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+      />;
   }
 
   render() {
