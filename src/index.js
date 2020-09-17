eff655bf2f1c455ed4810e6fb7c683b4d22f7966
wolf3d <oldcar@inbox.lv>
2020-09-18 00:10:51 +0300

    we _should_ see a list of the moves

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ class Board extends React.Component {
 
   render() {
     return (
-      <div>        
+      <div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -70,6 +70,18 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, map) => {
+      const desc = move ?
+      'Go to move #' + move :
+      'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -86,7 +98,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
