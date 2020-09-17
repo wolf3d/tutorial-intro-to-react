0b668659ccdb3a0dc441eab349cc0f6365889eb1
wolf3d <oldcar@inbox.lv>
2020-09-17 22:42:28 +0300

    instruct each individual Square about its current value

--- src/index.js
+++ src/index.js
@@ -25,11 +25,11 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null),      
+      squares: Array(9).fill(null),
     };
   }
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} />;
   }
 
   render() {
